55f83c78d18cc0c63c2199c72aa587fac0eadcc0
soulofmachines <soulofmachines@noreply.github.com>
2014-08-08 09:46:43 +0400

    (asound) show ERR# on errors

--- set_asound.cpp
+++ set_asound.cpp
@@ -17,14 +17,35 @@ int set_asound (barconfig &myconfig, bool json) {
 	snd_mixer_t *handle;
 	snd_mixer_selem_id_t *selem_id;
 	const snd_mixer_selem_channel_id_t selem_channel = SND_MIXER_SCHN_FRONT_LEFT;
-	snd_mixer_open (&handle, 0);
-	snd_mixer_attach (handle, myconfig.card.c_str());
-	snd_mixer_selem_register (handle, NULL, NULL);
-	snd_mixer_load (handle);
+	if (snd_mixer_open (&handle, 0) < 0) {
+		json_object_object_add(myconfig.json_output, "full_text", json_object_new_string ((char *)("ERR0")));
+		snd_mixer_close(handle);
+		return 0;
+		}
+	if (snd_mixer_attach (handle, myconfig.card.c_str()) < 0) {
+		json_object_object_add(myconfig.json_output, "full_text", json_object_new_string ((char *)("ERR1")));
+		snd_mixer_close(handle);
+		return 0;
+		}
+	if (snd_mixer_selem_register (handle, NULL, NULL) < 0) {
+		json_object_object_add(myconfig.json_output, "full_text", json_object_new_string ((char *)("ERR2")));
+		snd_mixer_close(handle);
+		return 0;
+		}
+	if (snd_mixer_load (handle) < 0) {
+		json_object_object_add(myconfig.json_output, "full_text", json_object_new_string ((char *)("ERR3")));
+		snd_mixer_close(handle);
+		return 0;
+		}
 	snd_mixer_selem_id_alloca (&selem_id);
 	snd_mixer_selem_id_set_index (selem_id, 0);
 	snd_mixer_selem_id_set_name (selem_id, myconfig.device.c_str());
 	snd_mixer_elem_t *elem = snd_mixer_find_selem (handle, selem_id);
+	if (!elem) {
+		json_object_object_add(myconfig.json_output, "full_text", json_object_new_string ((char *)("ERR4")));
+		snd_mixer_close(handle);
+		return 0;
+		}
 	snd_mixer_selem_get_playback_switch (elem, selem_channel, &volume_mute);
 	if (volume_mute == 1) {
 		snd_mixer_selem_get_playback_volume(elem, selem_channel, &volume_cur);
