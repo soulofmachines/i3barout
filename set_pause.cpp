bcde37598084e91fa2a613d9829ac86a88bf515a
soulofmachines <soulofmachines@noreply.github.com>
2014-09-21 02:34:28 +0400

    set pause update & add ignore file

--- set_pause.cpp
+++ set_pause.cpp
@@ -1,10 +1,8 @@
 #include "set_pause.hpp"
-#include <time.h>
+#include <unistd.h>
 
 using namespace std;
 
 void set_pause (unsigned int value) {
-    struct timespec timeout = {value};
-    struct timespec timenout;
-    nanosleep (&timeout, &timenout);
+    sleep (value);
 }
