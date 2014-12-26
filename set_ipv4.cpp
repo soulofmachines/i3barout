1871691a3478b2770d4501b7cb4051c049e0300a
soulofmachines <mecha@Asuka.mecha>
2014-12-26 18:53:37 +0300

    ipv4 no interface error

--- set_ipv4.cpp
+++ set_ipv4.cpp
@@ -12,14 +12,19 @@ int set_ipv4 (bar_config &my_bar_config) {
     fd = socket (AF_INET, SOCK_DGRAM, 0);
     strncpy (ifr.ifr_name, my_bar_config.device.c_str(), IFNAMSIZ-1);
     ioctl (fd, SIOCGIFADDR, &ifr);
-    close (fd);
-    my_bar_config.integer = 0;
-    my_bar_config.output = inet_ntoa (((struct sockaddr_in *)(&ifr.ifr_addr))->sin_addr);
-    if (my_bar_config.output == "0.0.0.0") {
-        return_value = -1;
+    if (ioctl (fd, SIOCGIFFLAGS, &ifr) != -1) {
+        if ((ifr.ifr_flags&IFF_RUNNING) != IFF_RUNNING) {
+            return_value = 1;
+            goto close;
+        }
+    } else {
+        return_value = 2;
         goto close;
     }
+    my_bar_config.integer = 0;
+    my_bar_config.output = inet_ntoa (((struct sockaddr_in *)(&ifr.ifr_addr))->sin_addr);
     return_value = 0;
 close:
+    close (fd);
     return return_value;
 }
